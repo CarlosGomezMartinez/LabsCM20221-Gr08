47f46d4de5b76e71da7c794b305d2b7da416750e
CarlosGomezMartinez <carlos.gomez35@udea.edu.co>
2022-07-13 22:53:42 -0500

    Se agregan las validaciones de los campos y el registro en logcat

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20221/lab1/ContactDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20221/lab1/ContactDataActivity.kt
@@ -2,10 +2,55 @@ package co.edu.udea.compumovil.gr08_20221.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class ContactDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_data)
+        onClickNextButtonContactData()
+    }
+
+    private fun onClickNextButtonContactData(){
+        val nextButton2: Button = findViewById(R.id.button_next2)
+
+        //Campos de la actividad
+        val phone : EditText = findViewById(R.id.textInput_phone)
+        val email : EditText = findViewById(R.id.textInput_email)
+        val country : EditText = findViewById(R.id.textInput_country)
+        val city : EditText = findViewById(R.id.textInput_city)
+        val address : EditText = findViewById(R.id.textInput_address)
+
+        //Campos que llegan en el intent
+        val bundle = intent.extras
+        val name = bundle?.getString("name")
+        val gender = bundle?.getString("gender")
+        val birthdate = bundle?.getString("birthdate")
+        val schoolarGrade = bundle?.getString("schoolarGrade")
+
+        nextButton2.setOnClickListener {
+            if (phone.text.isNotEmpty() && email.text.isNotEmpty() && country.text.isNotEmpty()){
+                Log.d("*", "INFORMACION PERSONAL: ")
+                Log.d("Nombre completo: ", name.toString())
+                Log.d("Género: ", gender.toString())
+                Log.d("Nació el: ", birthdate.toString())
+                Log.d("Grado de escolaridad: ", schoolarGrade.toString())
+                Log.d("*", "INFORMACION DE CONTACTO")
+                Log.d("Teléfono: ", phone.text.toString())
+                Log.d("Dirección: ", address.text.toString())
+                Log.d("Email: ", email.text.toString())
+                Log.d("País: ", country.text.toString())
+                Log.d("Ciudad: ", city.text.toString())
+            } else {
+                fieldsError()
+            }
+        }
+    }
+
+    private fun fieldsError() {
+        Toast.makeText(this, "Existen campos obligatorios sin llenar", Toast.LENGTH_LONG).show()
     }
 }
--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20221/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20221/lab1/PersonalDataActivity.kt
@@ -3,16 +3,66 @@ package co.edu.udea.compumovil.gr08_20221.lab1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.*
+import androidx.core.view.isNotEmpty
 
 class PersonalDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_activity)
+        onClickNextButtonPersonalData()
+    }
 
-        findViewById<Button>(R.id.button_next).setOnClickListener {
-            val intent = Intent(this, ContactDataActivity::class.java)
-            startActivity(intent)
+    private fun onClickNextButtonPersonalData(){
+        val nextButton : Button = findViewById(R.id.button_next)
+
+        //Campos para el nombre
+        val firstName : EditText = findViewById(R.id.textInput_firstname)
+        val lastName : EditText = findViewById(R.id.textInput_lastname)
+        val fullName : String = firstName.text.toString() + " " + lastName.text.toString()
+
+        //Verificacion de los radioButton
+        val radioMale : RadioButton = findViewById(R.id.radio_male)
+        val radioFemale : RadioButton = findViewById(R.id.radio_female)
+        val gMale = radioMale.isChecked
+        val gFemale = radioFemale.isChecked
+        lateinit var gender: String
+        if(gMale){
+            gender = "Masculino"
+        }  else if (gFemale) {
+            gender = "Femenino"
+        } else {
+            gender = "No especificado"
         }
+
+        //Seleccion de nivel de escolaridad
+        val schoolarGrade : Spinner = findViewById(R.id.spinner_school_grade)
+        val grade: String = schoolarGrade.selectedItem.toString()
+
+        //Seleccion de fecha de nacimiento
+        val birthDate : DatePicker = findViewById(R.id.picker_date_birth)
+        val birthYear = birthDate.year
+        val birthMonth = birthDate.month
+        val birthDay = birthDate.dayOfMonth
+        val dateBirth = birthDay.toString() + "/" + birthMonth.toString() + "/" + birthYear.toString()
+
+        //Se pasan los datos en el intent
+        nextButton.setOnClickListener {
+            if(firstName.text.isNotEmpty() && lastName.text.isNotEmpty() && birthDate.isNotEmpty()){
+                val intent = Intent(this, ContactDataActivity::class.java)
+                intent.putExtra("name", fullName)
+                intent.putExtra("gender", gender)
+                intent.putExtra("birthdate", dateBirth)
+                intent.putExtra("schoolarGrade", grade)
+                startActivity(intent)
+            } else {
+                fieldsError()
+            }
+        }
+    }
+
+
+    private fun fieldsError() {
+        Toast.makeText(this, "Existen campos obligatorios sin llenar", Toast.LENGTH_LONG).show()
     }
 }
